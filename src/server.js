95cfb52d3a75852a152abca029435bfda79aef2d
rhercb <rhercb@gmail.com>
2021-08-04 07:13:18 +0300

    Serving html page via express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.get('/', function (req, res) {
-  res.send('Some dummy content');
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf8');
+  res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function () {
